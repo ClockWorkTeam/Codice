2b3b861acbce77e31e64a7c5a15ba67753d13e35
alabruna <agostino.labruna@gmail.com>
2013-03-27 11:43:43 +0100

    cambiata generazione stringa in ContactsManager

--- src/server/functionmanager/ContactsManager.java
+++ src/server/functionmanager/ContactsManager.java
@@ -5,13 +5,21 @@ import java.util.Vector;
 import server.shared.User;
 
 public class ContactsManager {
+	/**genera la stringa dei contatti seguendo questo schema:
+	*  { size: N,
+	*    contact1: { username: x, name: y, surname: w, IP: N.N.N.N },
+	*    contact2: { ... },
+	*    contact3: { ... }
+	*  } 
+	*/
 	public String getAllContacts(Vector<User> users){
-		String contacts="{ size :"+users.size()+",";
+		String contacts="{ size: "+users.size()+",";
 		for(int i=0; i<users.size(); i++){
-			contacts+=" username"+i+": "+users.get(i).getUsername()+",";
-			contacts+=" name"+i+": "+users.get(i).getName()+",";
-			contacts+=" surname"+i+": "+users.get(i).getSurname()+",";
-			contacts+=" IP"+i+": "+users.get(i).getIP()+",";
+			contacts+=" contact"+i+
+				": { username: "+users.get(i).getUsername()+
+				", name: "+users.get(i).getName()+
+				", surname: "+users.get(i).getSurname()+
+				", IP: "+users.get(i).getIP()+" }";
 		}
 		contacts+="}";
 		return contacts;
