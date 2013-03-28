83c7db30da221c1403b8465e2712f31e11d32ae2
Liquid90 <g.bain@libero.it>
2013-03-28 13:51:46 +0100

    CallListener

---
+++ src/server/transfer/CallListener.java
@@ -0,0 +1,53 @@
+package server.transfer;
+
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.jwebsocket.api.WebSocketConnector;
+import org.jwebsocket.api.WebSocketPacket;
+import org.jwebsocket.config.JWebSocketConfig;
+import org.jwebsocket.factory.JWebSocketFactory;
+import org.jwebsocket.kit.RawPacket;
+import org.jwebsocket.kit.WebSocketServerEvent;
+import org.jwebsocket.listener.WebSocketServerTokenEvent;
+import org.jwebsocket.listener.WebSocketServerTokenListener;
+import org.jwebsocket.server.TokenServer;
+import org.jwebsocket.token.Token;
+
+import server.ServerMyTalk;
+
+
+public class CallListener implements WebSocketServerTokenListener {
+	TokenServer tokenServer;
+	
+    public void setTokenServer(ServerMyTalk server) {
+        tokenServer=server.getTokenServer();
+    }
+
+    public void processToken(WebSocketServerTokenEvent event, Token token) {
+   		String type= token.getString("type");
+   		WebSocketPacket wspacket=null;
+  		System.out.println("è arrivato un messaggio" + token.toString());
+   		if(type.equals("call"))
+   		{
+   			wspacket=new RawPacket(token.toString());
+   			sendPacket(wspacket, event);
+   		}
+  		
+    }
+
+    public void processClosed(WebSocketServerEvent arg0) {
+    }
+
+    public void processOpened(WebSocketServerEvent event) {
+        System.out.println("***********Client '" + event.getSessionId()
+                + "' connected.*********");
+    }
+
+    public void sendPacket(WebSocketPacket packet, WebSocketServerEvent event){
+		event.sendPacket(packet); 
+    }
+    public void processPacket(WebSocketServerEvent event, WebSocketPacket packet) {      
+    }
+}
